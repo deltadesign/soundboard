d5df298bf21d5f8932839458cf6e7e831ae6cb99
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-11-23 18:08:45 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => {key.addEventListener('transitionend', removeTransition)})
+
+window.addEventListener('keydown', keyPush)
+
+function keyPush(e){
+  const key = e.key;
+  const element = document.getElementById(`${key}`);
+  const audio = document.querySelector(`audio[data-key="${key}"]`)
+  
+  if(!element) {
+    return
+  } else {
+    element.classList.add('keyed')
+    audio.currentTime = 0;
+    audio.play();
+  }
+}
+
+ function removeTransition(e) {
+  if(e.propertyName !== 'transform') {
+    return
+  } else {
+    this.classList.remove('keyed')
+  }
+}
