35e2e8b7d9edacf47f632b3632e9951dbb81e5a9
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-11-23 19:18:58 +0000

    mobile friendly

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const keys = document.querySelectorAll('.key');
 keys.forEach((key) => {key.addEventListener('transitionend', removeTransition)})
 
 window.addEventListener('keydown', keyPush)
+document.addEventListener('click', clicker)
+
+function clicker(e){
+  const key = e.target.id;
+
+  const element = document.getElementById(`${key}`);
+  const audio = document.querySelector(`audio[data-key="${key}"]`)
+  
+  if(!element) {
+    return
+  } else {
+    element.classList.add('keyed')
+    audio.currentTime = 0;
+    audio.play();
+  }
+}
 
 function keyPush(e){
   const key = e.key;
